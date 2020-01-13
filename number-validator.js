9e150f018911ddf7ff2293d5ad069fb66a17fa49
simon.a.mora.suarez <simon.a.mora.suarez@accenture.com>
2020-01-13 17:52:38 -0300

    regex fr validation

--- number-validator.js
+++ number-validator.js
@@ -1,54 +1,54 @@
-function numberChecker(number){
-    if(number.length < 10){ 
-        console.log("not long enough: " + false);
-        return false;
-    }else if(number.length === 10){
-        if(number.match(/[0-9]/g).length === 10){
-            console.log("es un número porbablemente: "+ number);
-            return true;
-        }else{   
-            return false;
-        }
-    }else if(number.length === 11){
-        if(number.match(/^[1]/)){
-            if(number.match(/[0-9]/g).length === 11){
-                console.log("es un número porbablemente: "+ number);
-                return true;
-            }else{   
-                return false;
-            }
-        }else {
-            return false;
-        }
-    } else if(number.length > 11){
-        return false;
-    }
-}
+// function numberChecker(number){
+//     if(number.length < 10){ 
+//         console.log("not long enough: " + false);
+//         return false;
+//     }else if(number.length === 10){
+//         if(number.match(/[0-9]/g).length === 10){
+//             console.log("es un número porbablemente: "+ number);
+//             return true;
+//         }else{   
+//             return false;
+//         }
+//     }else if(number.length === 11){
+//         if(number.match(/^[1]/)){
+//             if(number.match(/[0-9]/g).length === 11){
+//                 console.log("es un número porbablemente: "+ number);
+//                 return true;
+//             }else{   
+//                 return false;
+//             }
+//         }else {
+//             return false;
+//         }
+//     } else if(number.length > 11){
+//         return false;
+//     }
+// }
 
-function telephoneCheck(str) {
-    // Good luck!
+// function telephoneCheck(str) {
+//     // Good luck!
 
-    let workStr = str.replace(/\W/gi, ' ');
-    let number = workStr.split(' ').join('');
+//     let workStr = str.replace(/\W/gi, ' ');
+//     let number = workStr.split(' ').join('');
     
-    console.log(workStr.split(' ').join(''));
+//     console.log(workStr.split(' ').join(''));
     
-    // console.log(str.split(/\W/));
-    if(str.match(/^\W/)){
-        if(str.match(/^\W/)[0] !== '(') {
-            return false;
-        }else{
-            if(str[4] !== ')'){ 
-                return false;
-            }else{
-                return numberChecker(number);
-            }
-        }
-    }else{
-        return numberChecker(number);           
-    }
+//     // console.log(str.split(/\W/));
+//     if(str.match(/^\W/)){
+//         if(str.match(/^\W/)[0] !== '(') {
+//             return false;
+//         }else{
+//             if(str[4] !== ')'){ 
+//                 return false;
+//             }else{
+//                 return numberChecker(number);
+//             }
+//         }
+//     }else{
+//         return numberChecker(number);           
+//     }
 
-  }
+//   }
 
 //   telephoneCheck("555-555-55A5");
 
@@ -83,4 +83,6 @@ telephoneCheck("(555)5(55?)-5555")
 //   console.log(telephoneCheck("(275)76227382")
 //   );
   
+let regex = /^[\(]{1}[0-9]{1,3}\){1}[-|\s]{0,1}[0-9]{3}[-|\s]{0,1}[0-9]{4}/;
+let anotherregex = /^[1]{1}[\(\s]{0,1}[0-9]{1,3}\){0,1}[-|\s]{0,1}[0-9]{3}[-|\s]{0,1}[0-9]{4}/;
   
